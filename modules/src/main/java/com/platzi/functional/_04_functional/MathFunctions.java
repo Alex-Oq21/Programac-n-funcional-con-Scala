7fa7033d388ae0e1a0a6524b12214b4cc1ba0d4f
Alexis <omaroquendo221@gmail.com>
2020-11-03 18:08:30 -0500

    Uso de Predicados

--- modules/src/main/java/com/platzi/functional/_04_functional/MathFunctions.java
+++ modules/src/main/java/com/platzi/functional/_04_functional/MathFunctions.java
@@ -1,6 +1,7 @@
 package com.platzi.functional._04_functional;
 
 import java.util.function.Function;
+import java.util.function.Predicate;
 
 public class MathFunctions {
     public static void main(String[] args) {
@@ -12,8 +13,27 @@ public class MathFunctions {
         };
         System.out.println(square.apply(5));
         System.out.println(squareFunction(10));
+
+        Function<Integer, Boolean> isOdd = x -> x % 2 == 1;
+        Predicate<Integer> isEven = x -> x %2 ==0;
+        isEven.test(4); // Debe devolver un true
+        //Función para determinar si un estudiante aprueba o no.
+        Predicate<Estudiante> isAprovedd = estudiante -> estudiante.getCalificacion() >=6.0;
+        Estudiante Alexis = new Estudiante(4);
+        System.out.println(isAprovedd.test(Alexis));
     }
      static int squareFunction(int x){
         return x*x;
+    }
+
+    static class Estudiante{
+        private double calificacion;
+
+        public Estudiante(double calificacion){
+            this.calificacion = calificacion;
+        }
+        public double getCalificacion(){
+            return calificacion;
+        }
     }
 }
