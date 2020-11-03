7916c141c27912f4441c53279c35f2427a928136
Alexis <omaroquendo221@gmail.com>
2020-11-03 18:40:05 -0500

    Consumer y Supplier

---
+++ modules/src/main/java/com/platzi/functional/_04_functional/CLIArgumentsUtils.java
@@ -0,0 +1,19 @@
+package com.platzi.functional._04_functional;
+
+import java.util.function.Consumer;
+import java.util.function.Supplier;
+
+public class CLIArgumentsUtils {
+    static void showHelp(CLIArguments cliArguments){
+        Consumer<CLIArguments> consumerHelper = cliArguments1 -> {
+            if (cliArguments.isHelp()){
+                System.out.println("Manual solicitado");
+            }
+        };
+        consumerHelper.accept(cliArguments);
+    }
+    static CLIArguments generateCLI(){
+        Supplier<CLIArguments> generatos = () -> new CLIArguments();
+        return generatos.get();
+    }
+}
